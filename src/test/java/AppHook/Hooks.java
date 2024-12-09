eccaa8da2874ccf2c5388d1fdff181e79ab7076f
Ela-Ram <ems112000@gmail.com>
2024-12-09 01:42:06 -0600

    updated ExcelReader and ConfigReader

--- src/test/java/AppHook/Hooks.java
+++ src/test/java/AppHook/Hooks.java
@@ -1,19 +1,36 @@
 package AppHook;
 
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 
+import Commons.ExcelReader;
 import DriverFactory.DriverConfig;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
 
 	static WebDriver driver;
 	
+	public static List<Map<String, String>> Login;
+    public static List<Map<String, String>> Register;
+	
 	@Before
-	public static void setupDriver()
+	public static void setupDriver(Scenario scenario)
 	{
 	driver = DriverConfig.getdriver();	
+	  try {
+          ExcelReader excelManager_hashmap = new ExcelReader();
+          Login = excelManager_hashmap.getData("src/test/resources/TestData/ExcelData.xlsx", "login");
+          Register = excelManager_hashmap.getData("src/test/resources/ExcelData.xlsx", "register");
+          System.out.println("Scenario Name: " + scenario.getName());
+        } catch (Exception e) {
+         e.printStackTrace();
+         throw new RuntimeException("Error initializing Excel data: " + e.getMessage());
+     }
 	}
 	
 	@After
--- src/test/java/Commons/ConfigReader.java
+++ src/test/java/Commons/ConfigReader.java
@@ -1,52 +1,25 @@
 package Commons;
 
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
 	
-	static Properties properties = null;
-	
-	public ConfigReader()
-	{
-		File Path = new File("./src/test/resources/TestData/Config.properties");
-		
-		try {
-			FileInputStream fis= new FileInputStream(Path);
-			properties = new Properties();
-			properties.load(fis);
-			
-			} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	public static String getBrowser()
-	{
-		String Browser = properties.getProperty("Browser");
-		return Browser;
-	}
-	
-	public static String getAppUrl()
-	{
-		String URL = properties.getProperty("URL");
-		return URL;
-	}
-	
-	public static String getUsername()
-	{
-		String Username = properties.getProperty("Username");
-		return Username;
-	}
-	
-	public static String getPassword()
-	{
-		String Password = properties.getProperty("Password");
-		return Password;
-	}
-	
-
-	
+	 public static Properties properties; 	 
+	 static {
+	 		try(FileInputStream fileinputstream = new FileInputStream("src/test/resources/TestData/config.properties")){
+	 			
+	 			properties = new Properties();
+	 			properties.load(fileinputstream);
+	 		} catch (FileNotFoundException e) {
+	 			e.printStackTrace();
+	 			} catch (IOException e1) {
+	 			e1.printStackTrace();
+	 		}	 	 
+	 	}
+	 public static String getProperty(String key) {
+	 	return properties.getProperty(key);
+	 }
 }
