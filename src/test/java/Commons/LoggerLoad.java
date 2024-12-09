65a56a3e531378813c1a6660e038640abb9830ee
Padmini <PadminiElangovan>
2024-12-08 22:13:57 -0600

    LoggerLoad class added

---
+++ src/test/java/Commons/LoggerLoad.java
@@ -0,0 +1,36 @@
+package Commons;
+
+import java.util.logging.LogManager;
+
+import com.mongodb.diagnostics.logging.Logger;
+
+public class LoggerLoad {
+private static Logger logger = LogManager.getLogger();
+	
+	public static void info(String message) 
+	{
+	logger.info(message);
+	}
+	
+	public static void warn(String message)
+	{
+	logger.warn(message);
+	}
+	
+	public static void error(String message)
+	{
+	logger.error(message);
+	}
+	
+	public static void fatal(String message)
+	{
+	logger.fatal(message);
+	}
+	
+	public static void debug(String message)
+	{
+	logger.debug(message);
+	}
+
+
+}
